bcea6aba33f8d2f6d8737e2876d4034202b7f50e
Gilmore <144559541+gilmore-ec@users.noreply.github.com>
2024-09-15 18:25:07 -0400

    Added: -text files -generator -automation script

---
+++ generator.py
@@ -0,0 +1,7 @@
+def main():
+
+    filename = 'home.txt'
+    file = open(filename,"r")
+
+if __name__ == "__main__":
+    main()
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+chmod +x generator.py
+python3 generator.py
